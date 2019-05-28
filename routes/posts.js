0191b3d599cbadcb9cb80b40deff5e876bfdac32
Anuj <anuj1072538@gmail.com>
2019-05-28 10:07:29 +0530

    added authentication to posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const passport  = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+// second level of check not allowing the user to post if he is not authenticated
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports = router;
