58b15f1901b8f42e76d5f9070036b0413814967e
Anuj <anuj1072538@gmail.com>
2019-05-28 09:53:22 +0530

    display user on the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+    // Post.find({},function(err,posts){
+    //     if(err){console.log('err in fetching the posts');return;}
+    //     return res.render('home', {
+    //         title: "Codeal | Home",
+    //         posts:posts
+    //     });
+    // })
+    //finding all the post and populating user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err){console.log('err in fetching the posts');return;}
+            return res.render('home', {
+                title: "Codeal | Home",
+                posts:posts
+            });
+    });      
+    
 }
 
 // module.exports.actionName = function(req, res){}
